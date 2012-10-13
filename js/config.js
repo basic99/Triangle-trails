a40d695310da01779159e435d6488f466a831f20
David Beckwith <dbitsolutions@gmail.com>
2012-10-13 17:05:00 -0400

    first working update from project checkout

--- js/config.js
+++ js/config.js
@@ -1,23 +1,35 @@
 var config = {
     "web_service_base": "http://maps.co.mecklenburg.nc.us/rest/",
-    "default_map_center": [35.270, -80.837],
-    "default_map_zoom": 10,
+    "geoserver_base": "http://jimserver.net/geoserver",//TD
+    "default_map_center": [35.853, -78.746],
+    "default_map_zoom": 12,
     "default_map_min_zoom": 10,
     "default_map_max_zoom": 18,
-    "base_map_layers": [{
-        "id": "meckbase",
-        "name": "Mecklenburg Base Map",
-        "attribution": "Mecklenburg County Base Map",
-        "wmsurl": "http://maps.co.mecklenburg.nc.us/mbtiles/mbtiles-server.php?db=meckbase.mbtiles&z={z}&x={x}&y={y}.png",
-        "maxZoom": 18,
-        "minZoom": 9,
-        "isVisible": true,
-        "isBaseLayer": true
-    }, {
+    "base_map_layers": [ {
         "id": "osm",
         "name": "Open Street Map",
         "attribution": "Provided by Tiles@Home",
         "wmsurl": "http://tile.openstreetmap.org/{z}/{x}/{y}.png",
+        "maxZoom": 18,
+        "minZoom": 9,
+        "isVisible": false,
+        "isBaseLayer": true
+    },
+    {
+        "id": "google_sat",
+        "name": "Google Satellite",
+        "type": google.maps.MapTypeId.SATELLITE,
+        "wmsurl": "",
+        "maxZoom": 18,
+        "minZoom": 9,
+        "isVisible": false,
+        "isBaseLayer": true
+    },
+    {
+        "id": "google_ter",
+        "name": "Google Terrain",
+        "type": google.maps.MapTypeId.TERRAIN,
+        "wmsurl": "",
         "maxZoom": 18,
         "minZoom": 9,
         "isVisible": false,
@@ -37,11 +49,11 @@ var config = {
         "isVisible": false,
         "isBaseLayer": false
     }, {
-        "id": "environment",
-        "name": "Environmental Layers",
-        "attribution": "Provided by Mecklenburg County GIS",
-        "wmsurl": "http://maps.co.mecklenburg.nc.us/geoserver/wms",
-        "layers": "postgis:view_regulated_floodplains,postgis:landfills,postgis:mpl_sites,postgis:water_quality_buffers,postgis:proposed_thoroughfares,postgis:air_pollution_facilities",
+        "id": "umstead_trails",
+        "name": "Umstead Park Trails",
+        //"attribution": "Provided by Mecklenburg County GIS",
+        "wmsurl": "http://jimserver.net/geoserver/opengeo/wms",
+        "layers": "umstead_named_trails,umstead_line",
         "format": "image/png",
         "transparent": true,
         "opacity": 0.5,
@@ -49,19 +61,5 @@ var config = {
         "minZoom": 15,
         "isVisible": false,
         "isBaseLayer": false
-    }, {
-        "id": "impervious_surface",
-        "name": "Impervious Surface",
-        "attribution": "Mecklenburg County GIS",
-        "wmsurl": "http://meckmap.mecklenburgcountync.gov/ArcGIS/services/impervious_surface_sm/MapServer/WMSServer",
-        "layers": "0,1",
-        "format": "image/png",
-        "transparent": true,
-        "opacity": 0.5,
-        "maxZoom": 18,
-        "minZoom": 17,
-        "isVisible": false,
-        "isBaseLayer": false,
-        "projection": "EPSG:102113"
     }]
 };
